2d91486de4971e5e5e7f99af634bafc173e1a5e6
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-05-01 13:21:48 +0530

    numbers and math

--- basics/nums_math.js
+++ basics/nums_math.js
@@ -1,5 +1,17 @@
 const score=400
-//console.log(ScrollTimeline)
+//console.log(score)
 
 const balance=new Number(100)
 //console.log(balance)
+
+//console.log(balance.toString())
+
+// console.log(Math.random()*10)
+// console.log(Math.floor(Math.random()*10))
+// console.log(Math.ceil(Math.random()*10))
+
+const min=10
+const max=20
+//console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
