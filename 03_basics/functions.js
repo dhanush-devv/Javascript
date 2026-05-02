8ccb4d29db5c38650764abd5ca493b14debd2645
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-05-02 19:11:19 +0530

    functions with object and array

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -23,5 +23,32 @@
 // function greeting(name='Virat Kohli') {
 //     console.log(`Hello ${name}`)
 // }
-
 // greeting()
+
+
+// function calculateCartPrice(...num) {
+//     console.log(num)
+    
+// }
+
+// // calculateCartPrice(2,3,4)
+
+// const user={
+//     name:"Dhanush",
+//     price:49
+// }
+
+// function handleObject(anyobject) {
+//     console.log(`User name is ${anyobject.name} and price is ${anyobject.price}`)
+// }
+// handleObject(user)
+
+
+const myNewArray=[100,200,300]
+
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray))
+
