56dd38fa9fa02a368703c769ac93ab32d291a66e
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-05-02 19:48:09 +0530

    scope

---
+++ 03_basics/scopes.js
@@ -0,0 +1,19 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+
+// if (true) {
+//     let a = 10
+//     const b = 20
+//     var c = 30
+// }
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+//let , const block scoped , var global scoped
