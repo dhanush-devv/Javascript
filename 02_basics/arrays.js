6956d58cf6c62fc699b0f6d77665f6ca2e71185a
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-05-02 07:57:27 +0530

    arrays

---
+++ 02_basics/arrays.js
@@ -0,0 +1,31 @@
+//arrays
+
+const myArr=[1,2,3,4,5]
+// console.log(myArr[1])
+
+const myArr2=new Array(1,2,3,4)
+// console.log(myArr2)
+
+
+//array methods
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(0)
+// myArr.shift()
+
+
+
+
+// myArr.forEach((myArr)=>{
+//     console.log(myArr)
+// })
+// console.log(myArr)
+
+console.log("A",myArr)
+
+const myA1=myArr.slice(1,3)
+console.log(myA1)
+const myA2=myArr.splice(1,3)
+console.log(myA2)
