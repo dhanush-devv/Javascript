3f12afbe942b191434994115917355a7809c9f6f
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-05-02 18:52:57 +0530

    functions

---
+++ 03_basics/functions.js
@@ -0,0 +1,27 @@
+// function greeting(name) {
+//     console.log(`Hello ${name}`)
+    
+// }
+
+// greeting("Dhanush")
+
+// function addTwoNumber(a,b) {
+//     return a+b
+    
+// }
+// const result=addTwoNumber(2,3)
+// console.log(result)
+
+// const addTwoNumber=function(a,b) {
+//     return a+b
+    
+// }
+// const result=addTwoNumber(2,3)
+// console.log(result)
+
+
+// function greeting(name='Virat Kohli') {
+//     console.log(`Hello ${name}`)
+// }
+
+// greeting()
