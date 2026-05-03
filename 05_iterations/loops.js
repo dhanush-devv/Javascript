93ad0a357b44b07ccd9b6a4fa1e335a3cde324f3
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-05-03 16:59:36 +0530

    loops

---
+++ 05_iterations/loops.js
@@ -0,0 +1,26 @@
+//for
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     if(i===5){
+//         break;
+//     }
+//     console.log(i)
+    
+// }
+
+// let i=0
+// while(i<=10){
+//     console.log(i);
+//     i++
+// }
+
+// let i=0;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i<10);
