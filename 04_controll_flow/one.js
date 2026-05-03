598845afad38a66fa5f578db3493d7aa9d1cf7e4
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-05-03 14:03:04 +0530

    control flow

---
+++ 04_controll_flow/one.js
@@ -0,0 +1,28 @@
+// let age=18
+
+// if(age>18){
+//     console.log('Adult')
+// }else{
+//     console.log('Young')
+// }
+
+//let age=18
+// age>18?console.log("Adult"):console.log("Young")
+
+// let month=3
+// switch (month) {
+//     case 1:
+//         console.log("January")
+//         break;
+//     case 2:
+//         console.log("feb")
+//         break;
+//     case 3:
+//         console.log("march")
+//         break;
+//     case 2:
+//         console.log("april")
+//         break;
+//     default:
+//         console.log('Default')    
+//}
