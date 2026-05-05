a63ac9bbf2968e1a6d66f5eb3d414d85513425f5
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-05-05 18:59:05 +0530

    commit

--- projects/2.js
+++ projects/2.js
@@ -1,20 +1,21 @@
 const form = document.querySelector('form');
 form.addEventListener('submit', function (e) {
   e.preventDefault();
-const height = document.getElementById('height').value;
-const weight = document.getElementById('weight').value;
-const results = document.querySelector('#results');
-// console.log(height);
-// console.log(weight);
-// const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-// results.innerHTML=`<span>${bmi}</span>`\
-if (height === '' || height < 0 || isNaN(height)) {
-  results.innerHTML = `Please give a valid height ${height}`;
-} else if (weight === '' || weight < 0 || isNaN(weight)) {
-  results.innerHTML = `Please give a valid weight ${weight}`;
-} else {
-  const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-  //show the result
-  results.innerHTML = `<span>${bmi}</span>`;
-}
-})
+  const height = parseInt(document.getElementById('height').value);
+  console.log(typeof height)
+  const weight = parseInt(document.getElementById('weight').value);
+  const results = document.querySelector('#results');
+  // console.log(height);
+  // console.log(weight);
+  // const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+  // results.innerHTML=`<span>${bmi}</span>`\
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please give a valid height ${height}`;
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //show the result
+    results.innerHTML = `<span>${bmi}</span>`;
+  }
+});
