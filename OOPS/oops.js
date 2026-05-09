00519337c75da1fc58002a7d66433dd8bbb8ee13
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-05-09 15:02:19 +0530

    OOPS

---
+++ OOPS/oops.js
@@ -0,0 +1,38 @@
+// //object literal
+
+// const user={
+//     //properties
+//     username:"Dhanush",
+//     loginCount:8,
+//     signedIn:true,
+
+//     getUserDetails:function(params) {
+//         console.log(this.username)
+//     }
+// }
+// console.log(user.getUserDetails());
+
+
+//contructor function
+
+// const promiseOne=new Promise()
+
+// const date = new Date()
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+
+const userOne = new User("Dhanush", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne);
+console.log(userTwo)
